e9b7a948f7d9e760291627dec6b31ec3a97fe938
adla <adam.lasota@ailleron.com>
2020-01-04 01:46:34 +0100

    dodanie inicjalizcji mapowania

--- crud/src/main/java/pl/lasota/tool/crud/service/base/BaseFullTextSearchService.java
+++ crud/src/main/java/pl/lasota/tool/crud/service/base/BaseFullTextSearchService.java
@@ -8,8 +8,11 @@ import pl.lasota.tool.crud.fulltextrepository.FullTextSearchRepository;
 import pl.lasota.tool.crud.fulltextrepository.SearchSpecificationFullText;
 import pl.lasota.tool.crud.fulltextrepository.SpecificationFullText;
 import pl.lasota.tool.crud.fulltextrepository.distributed.DistributeFullTextFactory;
+import pl.lasota.tool.crud.fulltextrepository.mapping.MustMapping;
+import pl.lasota.tool.crud.fulltextrepository.mapping.NotMustMapping;
+import pl.lasota.tool.crud.fulltextrepository.mapping.ShouldMapping;
+import pl.lasota.tool.crud.fulltextrepository.mapping.SortMapping;
 import pl.lasota.tool.crud.mapping.Mapping;
-
 import pl.lasota.tool.crud.service.SearchService;
 import pl.lasota.tool.crud.service.SpecificationProvider;
 
@@ -35,7 +38,12 @@ public class BaseFullTextSearchService<READING, MODEL extends EntityBase> implem
 
     @Override
     public SpecificationFullText providerSpecification(List<Field<?>> fields) {
+        MustMapping mustMapping = new MustMapping();
+        NotMustMapping notMustMapping = new NotMustMapping();
+        ShouldMapping shouldMapping = new ShouldMapping();
+        SortMapping sortMapping = new SortMapping();
 
-        return new SearchSpecificationFullText(new DistributeFullTextFactory(filter(fields), mustMapping, notMustMapping, shouldMapping, sortContext));
+        return new SearchSpecificationFullText(new DistributeFullTextFactory(filter(fields), mustMapping, notMustMapping,
+                shouldMapping, sortMapping));
     }
 }
