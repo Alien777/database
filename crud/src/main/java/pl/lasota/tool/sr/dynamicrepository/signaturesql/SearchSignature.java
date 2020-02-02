2280481b992234ad914288e0d892cc368fad8513
Adam Lasota <lasota.adam.92@gmail.com>
2020-02-02 15:43:25 +0100

    next iteration should be end of  SearchSignarure.class

--- crud/src/main/java/pl/lasota/tool/sr/dynamicrepository/signaturesql/SearchSignature.java
+++ crud/src/main/java/pl/lasota/tool/sr/dynamicrepository/signaturesql/SearchSignature.java
@@ -1,17 +1,27 @@
 package pl.lasota.tool.sr.dynamicrepository.signaturesql;
 
+import pl.lasota.tool.sr.field.CriteriaField;
+import pl.lasota.tool.sr.field.DistributeForField;
 import pl.lasota.tool.sr.field.Field;
+import pl.lasota.tool.sr.repository.DistributeForCriteria;
+import pl.lasota.tool.sr.service.SpecificationProvider;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class SearchSignature implements CreatableSql{
+public class SearchSignature implements CreatableSql {
 
     private final String tableName;
     private final List<Field<?>> fields;
+    private final DistributeForCriteria distribute;
 
     public SearchSignature(String tableName, List<Field<?>> fields) {
         this.tableName = tableName;
         this.fields = fields;
+        distribute = new DistributeForField(fields.stream()
+                .filter(field -> field instanceof CriteriaField<?>)
+                .map(field -> (CriteriaField<?>) field)
+                .collect(Collectors.toList()));
     }
 
     @Override
@@ -20,8 +30,18 @@ public class SearchSignature implements CreatableSql{
         StringBuilder sql = new StringBuilder();
         sql.append("SELECT * FROM ");
         sql.append(tableName);
-        sql.append(" WHERE ");
+        sql.append(" WHERE 1=1 ");
 
-        return format("SQL * FROM "+tableName);
+        return format("SQL * FROM " + tableName);
     }
+
+
+    private String createWhere()
+    {
+
+        List<CriteriaField<?>> and = distribute.and();
+        List<CriteriaField<?>> or = distribute.or();
+        return "";
+    }
+
 }
