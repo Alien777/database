255df64e83eb47734c62762b52a644f181d0001b
Adam Lasota <lasota.adam.92@gmail.com>
2020-02-22 01:00:55 +0100

    Added copySet and copyGet method for Access. Changed is for dozer to change reference during mapping, Hibernate persit to table each object, not distink by reference

--- crud/src/main/java/pl/lasota/tool/sr/security/EntitySecurity.java
+++ crud/src/main/java/pl/lasota/tool/sr/security/EntitySecurity.java
@@ -5,20 +5,25 @@ import pl.lasota.tool.sr.mapping.NotUpdating;
 import pl.lasota.tool.sr.repository.EntityBase;
 
 import javax.persistence.*;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @ToString(callSuper = true)
 public abstract class EntitySecurity extends EntityBase {
 
-    @OneToMany(cascade = {CascadeType.REMOVE, CascadeType.PERSIST}, fetch = FetchType.LAZY)
-    @JoinColumn(name = "entity_id")
-    @NotUpdating
-    private Set<Access> accesses;
-
     public EntitySecurity() {
     }
+
+    @OneToMany(cascade = {CascadeType.REMOVE, CascadeType.PERSIST}, fetch = FetchType.LAZY)
+    @NotUpdating
+    @JoinTable(name = "entity_access",
+            joinColumns = @JoinColumn(name = "entity_id"),
+            inverseJoinColumns = @JoinColumn(name = "access_id", unique = true)
+    )
+    private Set<Access> accesses;
 
     public Set<Access> getAccesses() {
         return accesses;
@@ -26,5 +31,15 @@ public abstract class EntitySecurity extends EntityBase {
 
     public void setAccesses(Set<Access> accesses) {
         this.accesses = accesses;
+    }
+
+    public void copySet(Set<Access> accesses) {
+        this.accesses = accesses.stream()
+                .map(access -> new Access(access.getName(), access.getRud())).collect(Collectors.toSet());
+    }
+
+    public Set<Access> copyGet() {
+        return this.accesses.stream()
+                .map(access -> new Access(access.getName(), access.getRud())).collect(Collectors.toSet());
     }
 }
